c85ad1385a9dda6abe4ed30b7824480fdf9dde27
Roos44 <selina.angelucci@gmail.com>
2022-04-12 17:03:28 +0200

    ADD: park objects EDIT: LanternAnimation and script

---
+++ Assets/Models/Park/Animations/Animation_Trigger_Lantern.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Animation_Trigger_Lantern : MonoBehaviour
+{
+
+    public float randomWait;
+    public bool playAnim = true;
+
+    void Update()
+    {
+        if (playAnim)
+            StartCoroutine(WaitAnim()); //wait random seconds for animation
+    }
+
+    public IEnumerator WaitAnim()
+    {
+        playAnim = false;
+        int randomWait = Random.Range(0, 10);
+        print("Time" + randomWait + " Play"); //debug                
+        yield return new WaitForSeconds(randomWait);
+        GetComponent<Animator>().Play("Sway_better"); ;  //Put your animation string
+        playAnim = true;
+    }
+}
